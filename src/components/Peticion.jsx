6f1c606fc2dbbe8a4a1bdae4ed579210ea139b20
Bernardo otero <bernardooteroaj@gmail.com>
2023-05-25 23:32:29 -0500

    segundo_definicionconst

--- src/components/Peticion.jsx
+++ src/components/Peticion.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 
 const Peticion = () => {
-
+    const [imagenes, setImagenes] = React.useState([])
+    const [count, setCount] = React.useState(0)
+    
+    const traerImagenes = async() =>{
+        try{
+            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=Z7eqnPapNHR8pvsr31asqhCaExOwKewEMfiFovjo&count=${count}`)
+            const {results} = await res.json()
+            setImagenes(results)
+        }catch(error){
+            console.log(error)
+        }   
+    }
+    const siguiente = () =>{
+        setCount(count+10)
+        traerImagenes()
+    }
     return (
     <div>Peticion</div>
         
