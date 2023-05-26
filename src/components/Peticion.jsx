ff2702d79e51a5f58774e26d26b6f9a7952030ad
Bernardo otero <bernardooteroaj@gmail.com>
2023-05-26 01:05:05 -0500

    cuarto_coreccionConsultayFunciones_atrasysiguiente

--- src/components/Peticion.jsx
+++ src/components/Peticion.jsx
@@ -3,27 +3,50 @@ import React from 'react'
 
 const Peticion = () => {
     const [imagenes, setImagenes] = React.useState([])
-    const [date, setDate] = React.useState("2000-06-16")
+    const [dateStart, setDateStart] = React.useState("1995-06-16")
+    const [dateEnd, setDateEnd] = React.useState("1995-06-25")
     
     const traerImagenes = async() =>{
         try{
-            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=Z7eqnPapNHR8pvsr31asqhCaExOwKewEMfiFovjo&start_date=1995-06-16&end_date=1995-06-30`)
+            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=Z7eqnPapNHR8pvsr31asqhCaExOwKewEMfiFovjo&start_date=${dateStart}&end_date=${dateEnd}`)
             const json = await res.json();
             const   results = json
-            setImagenes(results)     
-            console.log(imagenes);         
+            setImagenes(results)             
         }catch(error){
             console.log(error)
         }   
     }
-    const siguiente = () =>{
-        setDate(date)
-        traerImagenes()
-    }
-    const atras = () =>{
-        setDate(date)
-        traerImagenes()
-    }
+    const siguiente = () => {
+        const currentDateStart = new Date(dateStart);
+        const currentDateEnd = new Date(dateEnd);
+        currentDateStart.setDate(currentDateStart.getDate() + 10);
+        currentDateEnd.setDate(currentDateEnd.getDate() + 10);
+        const formattedDateStart = formatDate(currentDateStart);
+        const formattedDateEnd = formatDate(currentDateEnd);
+        setDateStart(formattedDateStart);
+        setDateEnd(formattedDateEnd);
+        traerImagenes();
+      };
+    
+      const atras = () => {
+        const currentDateStart = new Date(dateStart);
+        const currentDateEnd = new Date(dateEnd);
+        currentDateStart.setDate(currentDateStart.getDate() - 10);
+        currentDateEnd.setDate(currentDateEnd.getDate() - 10);
+        const formattedDateStart = formatDate(currentDateStart);
+        const formattedDateEnd = formatDate(currentDateEnd);
+        setDateStart(formattedDateStart);
+        setDateEnd(formattedDateEnd);
+        traerImagenes();
+      };
+
+      const formatDate = (date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+        return formattedDate;
+      };
     return (
         <div>
           <h1>IMAGENES DEL ESPACIO POR LA NASA</h1>
@@ -36,7 +59,7 @@ const Peticion = () => {
           ) : (
             imagenes?.map(({ date, title, explanation, url }) => (
               <div key={date}>
-                <h4>{date} - {title}</h4>
+                <h3>{date} - {title}</h3>
                 <img src={url} alt={title} />
                 <h4>{explanation}</h4>
               </div>
